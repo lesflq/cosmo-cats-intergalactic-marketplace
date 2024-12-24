746809d1607f0be5eaed152f49f759667222989a
lesflq <max.09seva@gmail.com>
2024-12-24 22:57:58 +0200

    Final changes

--- src/main/java/org/example/lab1/config/SecurityConfig.java
+++ src/main/java/org/example/lab1/config/SecurityConfig.java
@@ -1,26 +1,47 @@
 package org.example.lab1.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtValidators;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 @Configuration
 public class SecurityConfig {
-
     @Bean
-    @Order(2) // Перший пріоритет для GitHub аутентифікації
+    @Order(1) // Перший пріоритет для JWT аутентифікації
+    public SecurityFilterChain jwtSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher("/api/v1/**")
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/api/v1/**").permitAll() // Доступ для перегляду
+                        .requestMatchers(HttpMethod.POST, "/api/v1/products/**").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/products/**").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/products/**").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/order/entry/products").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/order/entry/products/{id}").authenticated()
+                        .anyRequest().denyAll()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwkSetUri("http://localhost:8080/.well-known/jwks.json"))
+                );
+        return http.build();
+    }
+    @Bean
+    @Order(2) // Другий пріоритет для GitHub аутентифікації
     public SecurityFilterChain githubSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/**").authenticated()
-                        .anyRequest().permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/**").permitAll() // Доступ для перегляду
+                        .requestMatchers(HttpMethod.POST, "/api/v1/categories/**").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/categories/**").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/categories/**").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/orders").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/orders").authenticated()
+                        .anyRequest().denyAll()
                 )
                 .oauth2Login(oauth2 -> oauth2
                         .loginPage("/oauth2/authorization/github")
@@ -29,29 +50,5 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    @Order(1) // Другий пріоритет для JWT аутентифікації
-    public SecurityFilterChain jwtSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .securityMatcher("/api/v1/**")
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/**").authenticated()
-                        .anyRequest().permitAll()
-                )
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwt -> jwt
-                                .jwkSetUri("http://localhost:8080/.well-known/jwks.json"))
-                );
-        return http.build();
-    }
-
-    @Bean
-    @Order(3) // Останній пріоритет для no-auth
-    public SecurityFilterChain noAuthSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .csrf(AbstractHttpConfigurer::disable);
-        return http.build();
-    }
 }
 
