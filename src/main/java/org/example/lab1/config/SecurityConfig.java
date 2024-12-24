2cd6f4dd25f4af9b92940ba6960c70d6fbda5972
lesflq <max.09seva@gmail.com>
2024-12-24 12:41:43 +0200

    Change switch-case logic with order annotation

--- src/main/java/org/example/lab1/config/SecurityConfig.java
+++ src/main/java/org/example/lab1/config/SecurityConfig.java
@@ -1,25 +1,53 @@
 package org.example.lab1.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 @Configuration
 public class SecurityConfig {
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Order(1) // Перший пріоритет для GitHub аутентифікації
+    public SecurityFilterChain githubSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/oauth2/authorization/github")
+                        .successHandler(new SavedRequestAwareAuthenticationSuccessHandler())
+                );
+        return http.build();
+    }
+
+    @Bean
+    @Order(2) // Другий пріоритет для JWT аутентифікації
+    public SecurityFilterChain jwtSecurityFilterChain(HttpSecurity http) throws Exception {
         http
+                .securityMatcher("/api/v1/**")
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/**").authenticated()
                         .anyRequest().permitAll()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt
-                                .jwkSetUri("https://localhost:8080/.well-known/jwks.json")
-                        )
+                                .jwkSetUri("https://localhost:8080/.well-known/jwks.json"))
                 );
-
+        return http.build();
+    }
+    @Bean
+    @Order(3) // Останній пріоритет для no-auth
+    public SecurityFilterChain noAuthSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 }
+
